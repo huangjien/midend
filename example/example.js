016667d0da7c7f8b5c6ecee25cb862235fef1a49
jien-huang <huang_jien@msn.com>
2020-10-26 16:49:45 +0000

    temp rest

--- example/example.js
+++ example/example.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import useMidEnd from '../src/index'
 
 const Root = () => {
-  const [name, setName] = useMidEnd('name', 'Jien')
+  const [name, setName] = useMidEnd('name', 'Jien', 'person', 'db')
   return (
     <div>
       <h2>hello {name}</h2>
